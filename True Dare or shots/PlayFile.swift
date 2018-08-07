6c8a8f099ab08ad5b631943b3bcf09cabc187120
diego <diego4ng@hotmail.com>
2018-08-07 15:09:22 -0500

    add view controller

---
+++ True Dare or shots/PlayFile.swift
@@ -0,0 +1,24 @@
+//
+//  PlayFile.swift
+//  True Dare or shots
+//
+//  Created by Diego on 07/08/18.
+//  Copyright © 2018 diego. All rights reserved.
+//
+
+import UIKit
+
+class PlayFile: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
