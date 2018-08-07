adce818cc6e372dc90b625ced7f6c33f14511af4
Isela Pacheco <diego@macbook-air-de-diego.local>
2018-08-06 20:56:05 -0500

    Initial Commit

---
+++ True Dare or shots/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  True Dare or shots
+//
+//  Created by diego on 06/08/18.
+//  Copyright © 2018 diego. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
