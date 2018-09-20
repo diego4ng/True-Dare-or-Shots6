cf4335069cdd9bb0bd1a109e0c15287470a2b2fa
diego <diego4ng@hotmail.com>
2018-09-20 12:25:18 -0500

    add more switch buttons for  settings files and images

--- True Dare or shots/SettingsFile.swift
+++ True Dare or shots/SettingsFile.swift
@@ -15,6 +15,20 @@ class SettingsFile: UIViewController {
     @IBOutlet weak var switch3: UISwitch!
     @IBOutlet weak var switch4: UISwitch!
     
+    @IBOutlet weak var switch1_reto: UISwitch!
+    @IBOutlet weak var switch2_reto: UISwitch!
+    @IBOutlet weak var switch3_reto: UISwitch!
+    @IBOutlet weak var switch4_reto: UISwitch!
+    
+    @IBOutlet weak var switch1_shot: UISwitch!
+    @IBOutlet weak var switch2_shot: UISwitch!
+    @IBOutlet weak var switch3_shot: UISwitch!
+    @IBOutlet weak var switch4_shot: UISwitch!
+    
+    
+    // ***************************Cuando los switches son presionados**************************************
+    
+    
     @IBAction func switch1tapped(_ sender: UISwitch) {
         print("el valor del switch 1 es: \(updateswitch1())")
         UserDefaults.standard.set(updateswitch1(), forKey:"valor1")
@@ -37,17 +51,64 @@ class SettingsFile: UIViewController {
         UserDefaults.standard.set(updateswitch4(), forKey:"valor4")
     }
     
+    
+    
+    @IBAction func switch1_reto_tapped(_ sender: UISwitch) {
+    }
+    
+    @IBAction func switch2_reto_tapped(_ sender: UISwitch) {
+    }
+    
+    @IBAction func switch3_reto_tapped(_ sender: UISwitch) {
+    }
+    
+    @IBAction func switch4_reto_tapped(_ sender: UISwitch) {
+    }
+    
+    
+    
+    @IBAction func switch1_shot_tapped(_ sender: UISwitch) {
+    }
+    
+    @IBAction func switch2_shot_tapped(_ sender: UISwitch) {
+    }
+    
+    @IBAction func switch3_shot_tapped(_ sender: UISwitch) {
+    }
+    
+    @IBAction func switch4_shot_tapped(_ sender: UISwitch) {
+    }
+    
+    
+    
+    //********************Inicializa los switches*****************+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         UserDefaults.standard.set(true, forKey:"valor1")
         UserDefaults.standard.set(true, forKey:"valor2")
         UserDefaults.standard.set(true, forKey:"valor3")
         UserDefaults.standard.set(true, forKey:"valor4")
+        UserDefaults.standard.set(true, forKey:"valor5")
+        UserDefaults.standard.set(true, forKey:"valor6")
+        UserDefaults.standard.set(true, forKey:"valor7")
+        UserDefaults.standard.set(true, forKey:"valor8")
+        UserDefaults.standard.set(true, forKey:"valor9")
+        UserDefaults.standard.set(true, forKey:"valor10")
+        UserDefaults.standard.set(true, forKey:"valor11")
+        UserDefaults.standard.set(true, forKey:"valor12")
         switch1.isOn = true
         switch2.isOn = true
         switch3.isOn = true
         switch4.isOn = true
-        
+        switch1_reto.isOn = true
+        switch2_reto.isOn = true
+        switch3_reto.isOn = true
+        switch4_reto.isOn = true
+        switch1_shot.isOn = true
+        switch2_shot.isOn = true
+        switch3_shot.isOn = true
+        switch4_shot.isOn = true
         
         //recuperarconfiguracion()
         // Do any additional setup after loading the view, typically from a nib.
@@ -125,7 +186,111 @@ class SettingsFile: UIViewController {
         return valor4
     }
     
-    // recupera solo la configuracion a la ultima vez que se uso
+    
+    func updateswitch5() -> Bool {
+        let valor4 :Bool
+        
+        if switch1_reto.isOn
+        {
+            valor4 = true
+        }else{
+            valor4 = false
+        }
+        return valor4
+    }
+    
+    func updateswitch6() -> Bool {
+        let valor4 :Bool
+        
+        if switch2_reto.isOn
+        {
+            valor4 = true
+        }else{
+            valor4 = false
+        }
+        return valor4
+    }
+    
+    func updateswitch7() -> Bool {
+        let valor4 :Bool
+        
+        if switch3_reto.isOn
+        {
+            valor4 = true
+        }else{
+            valor4 = false
+        }
+        return valor4
+    }
+    
+    
+    func updateswitch8() -> Bool {
+        let valor4 :Bool
+        
+        if switch4_reto.isOn
+        {
+            valor4 = true
+        }else{
+            valor4 = false
+        }
+        return valor4
+    }
+    
+    func updateswitch9() -> Bool {
+        let valor4 :Bool
+        
+        if switch1_shot.isOn
+        {
+            valor4 = true
+        }else{
+            valor4 = false
+        }
+        return valor4
+    }
+    
+    func updateswitch10() -> Bool {
+        let valor4 :Bool
+        
+        if switch2_shot.isOn
+        {
+            valor4 = true
+        }else{
+            valor4 = false
+        }
+        return valor4
+    }
+    
+    func updateswitch11() -> Bool {
+        let valor4 :Bool
+        
+        if switch3_shot.isOn
+        {
+            valor4 = true
+        }else{
+            valor4 = false
+        }
+        return valor4
+    }
+    
+    func updateswitch12() -> Bool {
+        let valor4 :Bool
+        
+        if switch4_shot.isOn
+        {
+            valor4 = true
+        }else{
+            valor4 = false
+        }
+        return valor4
+    }
+
+}
+
+
+
+
+
+// recupera solo la configuracion a la ultima vez que se uso
 //    func recuperarconfiguracion(){
 //        let switchon:Int = UserDefaults.standard.object(forKey: "valor1") as! Int
 //        if (switchon == 0) {
@@ -156,4 +321,3 @@ class SettingsFile: UIViewController {
 //        }
 //
 //}
-}
