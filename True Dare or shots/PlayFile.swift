ec1200db63103b277f716a7a3cf3decc3e57869d
diego <diego4ng@hotmail.com>
2018-09-20 15:02:24 -0500

    Add retos y verdades class

--- True Dare or shots/PlayFile.swift
+++ True Dare or shots/PlayFile.swift
@@ -30,75 +30,11 @@ class PlayFile: UIViewController {
     var switchestado3 : Bool? = nil
     var switchestado4 : Bool? = nil
     
-    let arrayverdadx = ["1.-Alguna vez has tenido una relación de una noche? Si es así ¿Cuántas has tenido?",
-                        "2.-¿De que carecteristica tuya eres demasiado consciente?",
-                        "3.-¿Cuando fue la primera vez que fumaste",
-                        "4.-¿Alguna vez has llorado durante un orgasmo?",
-                        "5.-¿Has fallado en seducir a alguien? Si es así cuantas veces?",
-                        "6.-¿Alguna vez has interrumpido mientras tus padres estaban teniendo sexo?",
-                        "7.-¿Cual es tu posición sexual favorita?",
-                        "8.-¿Alguna vez has sido sucia en un coche?",
-                        "9.-¿Romperías la relación de una pareja si eso te llevase a salir con la persona de tus sueños?",
-                        "10.-¿Qué tipo de borracho eres: feliz,social,difundes el amor,malvado, sexual,calmado,astuto",
-                        "11.-Recuerdas la primera vez que viste a un miembro del sexo opuesto(que no sean tus padres)desnudo?¿Qué te parecio?",
-                        "12.-Te afeitas el vello púbico",
-                        "13.-¿Cuanto dinero ganas?",
-                        "14.-Alguna vez has engañado a tu novi@?",
-                        "15.-¿Cuantas veces durante el dia piensas en el sexo?",
-                        "16.-Si fueras un alimento ¿Qué serías y cómo otras personas te comerían?",
-                        "17.-Si no eres gay, que persona presente sería más probable que te volviera gay",
-                        "18.-¿Alguna vez te has bañado desnudo en público?",
-                        "19.-Con qué tipo de persona te gustaría casarte algún dia? ",
-                        "20.-¿Qué comida te comerías todos los días si pudieras",
-                        "21.-¿Cuál es el sueño mas sucio que has tenido? Describe con detalle",
-                        "22.-¿Alguna vez has llegado al orgasmo con toda la ropa puesta?",
-                        "23.-¿Cuando te comenzaste a masturbar?",
-                        "24.-¿Recibes o das más?",
-                        "25.-¿Si tuvieras que matar a alguien ¿Quién sería?",
-                        "26.-¿Cuál es tu talento secreto?",
-                        "27.-¿Si tuvieras la oportunidad de volver a nacer como alguien más en este cuarto,¿quién sería?¿Por qué?",
-                        "28.-Entre todas las personas en esta sala, con qué persona lo hárias?",
-                        "29.-¿Alguna vez has intimado a alguien?",
-                        "30.-Pregunta a un jugador a tu derecha si ha tenido relaciones sexuales con alguien que conozcas, si es asi ¿quién era, cómo era? y darles una calificación.",
-                        "31.-Si le va a dar consejo de (no hagas lo que yo hice) a una persona mas joven ¿qué sería?",
-                        "32.-¿Cuál es tu mayor provocación sexual?",
-                        "33.-¿Cuál es tu más profunda, más oscura fantasía?",
-                        "34.-¿Alguna vez has usado lubricante?",
-                        "35.-¿Con quién en el gruoo tendrías sexo y ¿ por qué tendrias sexo con él o ella?",
-                        "36.-¿Cual es la cosa más loca que has hecho desnudo?",
-                        "37.-Redtube, Youporn o PornHub? ¿Cuál y porque..?",
-                        "38.-¿Cuál es tu idea de una cita perfecta?",
-                        "39.-¿Alguna vez has besado al hermano o hermana de un amigo?",
-                        "40.-¿Cuándo has amado a alguien que no te haya amado de vuelta?",
-                        "41.-¿Alguna vez has besado a alguien del mismo sexo? ¿Quien?",
-                        "42.-¿Alguna vez has acosado a alguien? Si lo has ¿Quién fue?",
-                        "43.-¿Cuando tuviste tu primer beso y cómo fue?",
-                        "44.-¿Has tomado la virginidad de multiples personas?",
-                        "45.-¿De que tienes miedo?",
-                        "46.-¿Cuál es el mayor número de veces que has tenido relaciones sexuales en el transcurso de un día",
-                        "47.-",
-                        "48.-",
-                        "49.-",
-                        "50.-",
-                        "51.-",
-                        "52.-",
-                        "53.-",
-                        "54.-",
-                        "55.-",
-                        "56.-",
-                        "57.-",
-                        "58.-",
-                        "59.-¿Cuál es el menor tiempo que has tardado en llegar a un orgasmo?",]
+    let arrayverdadx = ["A,B,C,D"]
+    let arrayverdad2 = ["1", "2", "3","4"]
+     let arrayverdad3 = ["-XX-","YY","-WW-","ZZ"]
+     let arrayverdad4 = ["arrayverdad4-11-", "arrayverdad4-22-", "arrayverdad4-33-","arrayverdad4-44-"]
     
-     let arrayverdad2 = ["verdad1", "verdad2", "verdad3","verdad4", "verdad5","verdad6","verdad1", "verdad2", "verdad3","verdad4", "verdad5","verdad6","verdad1", "verdad2", "verdad3","verdad4", "verdad5","verdad6","verdad1", "verdad2", "verdad3","verdad4", "verdad5","verdad6","verdad1", "verdad2", "verdad3","verdad4", "verdad5","verdad6"]
-     let arrayverdad3 = ["verdad1.0", "verdad2.0", "verdad3.0","verdad4.0", "verdad5.0","verdad6.0","verdad1.0", "verdad2.0", "verdad3.0","verdad4.0", "verdad5.0","verdad6.0","verdad1.0", "verdad2.0", "verdad3.0","verdad4.0", "verdad5.0","verdad6.0","verdad1.0", "verdad2.0", "verdad3.0","verdad4.0", "verdad5.0","verdad6.0","verdad1.0", "verdad2.0", "verdad3.0","verdad4.0", "verdad5.0","verdad6.0"]
-    
-    
-    let arrayretos = ["reto1", "reto2", "Reto3","Reto4", "Reto5","Reto 6"]
-
-    let arrayShot = ["Shot1", "shot2", "shot3","shot3", "shot4","shot5"]
-    
-     let arrayShot2 = ["Shot1", "shot2", "shot3","shot3", "shot4","shot5"]
   
     
     var arreglo_global_verdad = [String]()
