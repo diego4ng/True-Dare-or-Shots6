8a52a8b07c8c2b127c267cacb9b53ff6441147e7
diego <diego4ng@hotmail.com>
2018-09-24 14:22:43 -0500

    Add interface agregar reto

--- True Dare or shots/AddDare.swift
+++ True Dare or shots/AddDare.swift
@@ -8,18 +8,74 @@
 
 import UIKit
 
-class AddDare: UIViewController {
+extension UIColor {
+    static func color(_ red: Int, green: Int, blue: Int, alpha: Float) -> UIColor {
+        return UIColor(
+            red: 1.0 / 255.0 * CGFloat(red),
+            green: 1.0 / 255.0 * CGFloat(green),
+            blue: 1.0 / 255.0 * CGFloat(blue),
+            alpha: CGFloat(alpha))
+    }
+}
+
+
+class AddDare: UIViewController, CircleMenuDelegate {
     
+    @IBOutlet weak var boton_seleccionado: UILabel!
+    
+    //    let colors = [UIColor.redColor(), UIColor.grayColor(), UIColor.greenColor(), UIColor.purpleColor()]
+    let items: [(icon: String, color: UIColor)] = [
+        ("botton verdadX", UIColor(red: 255/255, green: 97/255, blue: 0/255, alpha: 1)),
+        ("botton RetoX", UIColor(red: 0/255, green: 105/255, blue: 163/255, alpha: 1)),
+        ("botton ShotX", UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1)),
+        ("adddateblack", UIColor(red: 0.51, green: 0.15, blue: 1, alpha: 1)),
+        ("adddateblack", UIColor(red: 1, green: 0.39, blue: 0, alpha: 1)),
+        ]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // add button
+                let button = CircleMenu(
+                    frame: CGRect(x: 160, y: 220, width: 50, height: 50),
+                    normalIcon:"PRUEBA2",
+                    selectedIcon:"PRUEBA1-2",
+                    buttonsCount: 3,
+                    duration: 2,
+                    distance: 120)
+        button.backgroundColor = UIColor.lightGray
+                button.delegate = self
+                button.layer.cornerRadius = button.frame.size.width / 4.0
+                view.addSubview(button)
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK: <CircleMenuDelegate>
+    
+    func circleMenu(_: CircleMenu, willDisplay button: UIButton, atIndex: Int) {
+        button.backgroundColor = items[atIndex].color
+        
+        button.setImage(UIImage(named: items[atIndex].icon), for: .normal)
+        
+        // set highlited image
+        let highlightedImage = UIImage(named: items[atIndex].icon)?.withRenderingMode(.alwaysTemplate)
+        button.setImage(highlightedImage, for: .highlighted)
+        button.tintColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.3)
     }
     
+    func circleMenu(_: CircleMenu, buttonWillSelected _: UIButton, atIndex: Int) {
+        print("button will selected: \(atIndex)")
+        if atIndex == 0{
+        boton_seleccionado.text = "Verdad"
+        }
+        if atIndex == 1{
+            boton_seleccionado.text = "Reto"
+        }
+        if atIndex == 2{
+            boton_seleccionado.text = "Shot"
+        }
+    }
     
+    func circleMenu(_: CircleMenu, buttonDidSelected _: UIButton, atIndex: Int) {
+        print("button did selected: \(atIndex)")
+    }
 }
 
