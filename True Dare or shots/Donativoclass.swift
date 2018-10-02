3e26bb70c1a3a5bf6f62d4fe5a18e021a69594a6
diego <diego4ng@hotmail.com>
2018-10-02 15:17:07 -0500

    agregar clase para app purchase

---
+++ True Dare or shots/Donativoclass.swift
@@ -0,0 +1,42 @@
+//
+//  Donativoclass.swift
+//  True Dare or shots
+//
+//  Created by Diego on 02/10/18.
+//  Copyright © 2018 diego. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Donativoclass: UIViewController,UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "StoreCell", for: indexPath) as! Storeitemcollectionviewcell
+        
+        return cell
+    }
+    
+    
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
