8aa684b947a8f950a629a042905a170b162b8a9d
diego <diego4ng@hotmail.com>
2018-10-13 17:05:53 -0500

    fix twitter button constrains

--- True Dare or shots/infofile.swift
+++ True Dare or shots/infofile.swift
@@ -20,5 +20,36 @@ class infofile: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func seguirdiego(_ sender: UIButton) {
+        
+        let twUrl: NSURL = NSURL(string: "twitter://user?screen_name=Diego4ng")!
+        let twUrlWeb: NSURL = NSURL(string: "https://twitter.com/Diego4ng")!
+        
+        if(UIApplication.shared.canOpenURL(twUrl as URL)) {
+            // If user twitter installed
+            UIApplication.shared.open(twUrl as URL, options: [:], completionHandler: nil)
+        } else {
+            // If user does not have twitter installed
+            UIApplication.shared.open(twUrlWeb as URL, options: [:], completionHandler: nil)
+        }
+        
+    }
+
+    
+    
+    @IBAction func seguirjoel(_ sender: UIButton) {
+        let twUrl: NSURL = NSURL(string: "twitter://user?screen_name=joel_lozanoA")!
+        let twUrlWeb: NSURL = NSURL(string: "https://twitter.com/joel_lozanoA")!
+        
+        if(UIApplication.shared.canOpenURL(twUrl as URL)) {
+            // If user twitter installed
+            UIApplication.shared.open(twUrl as URL, options: [:], completionHandler: nil)
+        } else {
+            // If user does not have twitter installed
+            UIApplication.shared.open(twUrlWeb as URL, options: [:], completionHandler: nil)
+        }
+        
+        
+    }
     
 }
